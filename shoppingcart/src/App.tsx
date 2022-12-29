38deac3bf4ca6bec4ddcae9203559c84af1c115b
Adarsh <adarshregmi1@gmail.com>
2022-12-29 23:34:56 +0530

    Contd.

--- shoppingcart/src/App.tsx
+++ shoppingcart/src/App.tsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
 import { useQuery } from "react-query";
-
+import Item from "./Item/Item";
 //here are the components
 import Drawer from '@mui/material/Drawer'
 import LinearProgress from "@mui/material/LinearProgress";
 import Grid from "@mui/material/Grid";
 import AddShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-import Badge from "@mui/material/Badge";
+import { Badge } from "@mui/material";
 
 import { Wrapper } from "./Appstyles";
 
@@ -14,7 +14,7 @@ import { Wrapper } from "./Appstyles";
 
 //first await for the fetch and the other one for convertingit to json
 //Types
-
+//query is used here to receive the data from the fake api 
 export type Shopitemtype = {
   id: number;
   category: string;
@@ -25,15 +25,37 @@ export type Shopitemtype = {
   amount: number;
 }
 
-const getproducts = async (): Promise<Shopitemtype> => await (await fetch('https://fakestoreapi.com/products')).json();
+const getproducts = async (): Promise<Shopitemtype[]> => await (await fetch('https://fakestoreapi.com/products')).json();
 
 
 const App = () => {
-  const { data, isLoading, error } = useQuery<Shopitemtype[]>('products', getproducts);
+  const { data, isLoading, error } = useQuery<Shopitemtype[]>('products', getproducts
+  );
   console.log(data)
-  return <div className="App">
-    Hello world im back
-  </div>
+
+
+  const carttotal = () => null;
+  const cartadd = (clicky: Shopitemtype) => null;
+  const cartremove = () => null;
+
+  if (isLoading) return <LinearProgress />
+
+  if (error) return <div> Error encountered </div>
+
+  //?cuzitmay be undefined
+  return (
+    <Wrapper>
+      <Grid container spacing={3}>
+
+        {data?.map(item => (
+          <Grid item key={item.id} xs={12} sm={4}>
+            <Item item={item} cartadd={cartadd} />
+          </Grid>
+        )
+        )}
+      </Grid>
+    </Wrapper>
+  )
 }
 
 export default App;
---
+++ shoppingcart/src/Item/Item.tsx
@@ -0,0 +1,28 @@
+import Button from '@mui/material/Button';
+// types
+import { Wrapper } from './Itemstyles';
+import { Shopitemtype } from '../App';
+
+
+
+
+
+type Props = {
+    item: Shopitemtype;
+    cartadd: (clicky: Shopitemtype) => void;
+}
+//type for react functional component
+
+const Item: React.FC<Props> = ({ item, cartadd }) => (
+    <Wrapper>
+        <img src={item.image} alt={item.title} />
+        <div>
+            <h3>{item.title}</h3>
+            <p>{item.description}</p>
+            <h3>Rs.{item.price}</h3>
+        </div>
+        <Button onClick={() => cartadd(item)}> Add </Button>
+    </Wrapper>
+);
+
+export default Item;
