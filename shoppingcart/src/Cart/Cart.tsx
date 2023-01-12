5468fbbc58e6299e89c6eca2de61a9abdaa6b027
Adarsh <adarshregmi1@gmail.com>
2023-01-12 19:30:50 +0530

    updates

--- shoppingcart/src/Cart/Cart.tsx
+++ shoppingcart/src/Cart/Cart.tsx
@@ -1,22 +1,22 @@
-import Cartitem from "../CartItem/Cartitem";
+import CartItem from "../CartItem/Cartitem";
 
 import { Wrapper } from "./Cartstyles";
 
 import { Shopitemtype } from "../App";
 
 type Props = {
-    Cartitems: Shopitemtype[];
+    cartItems: Shopitemtype[];
     addtocart: (clickeditem: Shopitemtype) => void;
     removefromcart: (id: number) => void;
 };
 
-const Cart: React.FC<Props> = ({ Cartitems, addtocart, removefromcart }) => {
+const Cart: React.FC<Props> = ({ cartItems, addtocart, removefromcart }) => {
     return (
         <Wrapper>
             <h2>Your Shopping Cart</h2>
-            {Cartitems.length === 0 ? <p>No items in cart.</p> : null}
-            {Cartitems.map(item => (
-                <Cartitem />
+            {cartItems.length === 0 ? <p>No items in cart.</p> : null}
+            {cartItems.map(item => (
+                <CartItem />
             ))}
         </Wrapper>
     )
---
+++ shoppingcart/src/CartItem/Cartitem.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@mui/material";
+//types
+
+import { Shopitemtype } from "../App";
+//styles
+import { Wrapper } from "./Cartitemstyles";
+const CartItem: React.FC = () => <div>Cart Item</div>
+
+
+export default CartItem;
